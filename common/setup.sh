4d07bf9a4e842b7d9e1620e46cf529cf47b3e434
root <rtlepple@gmail.com>
2021-04-21 16:15:32 +0000

    initial commit

--- common/setup.sh
+++ common/setup.sh
@@ -3,12 +3,19 @@
 ###########################################################################################################
 # import parameters and utility functions 
 ###########################################################################################################
+. common/input.properties
 . common/utils.sh
 
 ###########################################################################################################
 #	install postgresql
 ###########################################################################################################
 echo "functions loaded..."
+
+echo "Test that user & pass have been set"
+if [ "${}" ] || [ "${}" ]; then
+	log "Credentails have not been set.  Please update.  Exiting..."
+	exit 1
+fi
 
 echo "begin install postgresql..."
 install_postgres
