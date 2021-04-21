d08ff47988e5fdf8801c4bb902f497f38fcf798c
root <rtlepple@gmail.com>
2021-04-21 16:59:49 +0000

    initial commit

--- common/setup.sh
+++ common/setup.sh
@@ -6,22 +6,28 @@
 . common/input.properties
 . common/utils.sh
 
+# logging function
+log() {
+    echo -e "[$(date)] [$BASH_SOURCE: $BASH_LINENO] : $*"
+
 ###########################################################################################################
 #	install postgresql
 ###########################################################################################################
+
+
 echo "functions loaded..."
 
 echo "Test that user & pass have been set"
-if [ "${}" ] || [ "${}" ]; then
+if [[ -z "{$CLDR_REPO_USER}" ]] || [[ -z "{$CLDR_REPO_PASS}" ]]; then
 	log "Credentails have not been set.  Please update.  Exiting..."
 	exit 1
 fi
 
 echo "begin install postgresql..."
-install_postgres
+#install_postgres
 
 echo "database installed..."
 
 echo
 echo "install prereqs..."
-setup_prereqs
+#setup_prereqs
